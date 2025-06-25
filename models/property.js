ef3991629010792e0960b1dd90db68c9b66699b1
Immoleasf <Info@immoleaf.de>
2025-06-25 21:04:13 +0200

    Update property.js

--- models/property.js
+++ models/property.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
 
+const propertySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  zipCode: { type: String },
+  city: { type: String },
+  country: { type: String, default: 'Germany' },
+  type: { type: String, enum: ['house', 'apartment', 'commercial'], default: 'house' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Property', propertySchema);
