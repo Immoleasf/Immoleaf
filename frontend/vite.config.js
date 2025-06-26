30b37a6d6f62eee31f6a61715faf39cc3afe1f68
Immoleasf <Info@immoleaf.de>
2025-06-26 16:07:00 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // optional: React Dev-Server-Port
-  },
-  build: {
-    outDir: 'dist',
-  },
+    proxy: {
+      '/api': {
+        target: 'https://immoleaf-backend-h6f9g5bzc8aqh8f5.westeurope-01.azurewebsites.net',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
