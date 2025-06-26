8e06b5f1d2fc9fe43fc4f7600b180e28bbb9e249
Immoleasf <Info@immoleaf.de>
2025-06-26 17:20:07 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './components/Login'
-import Dashboard from './components/Dashboard'
+import Dashboard from './pages/Dashboard'
 
 export default function App() {
   return (
